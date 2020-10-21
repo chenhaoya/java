d037ecf669df22734d1502da8720fecc2c6fc3c2
chenhaoya <1627785181@qq.com>
2020-10-21 22:30:20 +0800

    2020-10-21

---
+++ src/ThankingInJava/Wind.java
@@ -0,0 +1,21 @@
+package ThankingInJava;
+/**
+* 向上转型
+* */
+class Instrument{
+    Instrument(){
+        System.out.println("我是构造");
+    }
+    public void play(){
+        System.out.println("play");
+    }
+    static void tune(Instrument i){
+        i.play();
+    }
+}
+public class Wind extends Instrument {
+    public static void main(String[] args) {
+        Wind flute =new Wind();
+        Instrument.tune(flute);
+    }
+}
