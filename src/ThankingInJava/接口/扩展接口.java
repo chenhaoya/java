ad5016128d038f0c92672b8e299120e39a85f448
chenhaoya <1627785181@qq.com>
2020-11-03 23:31:33 +0800

    2020.11.3

--- src/ThankingInJava/接口/扩展接口.java
+++ src/ThankingInJava/接口/扩展接口.java
@@ -1,5 +1,9 @@
 package ThankingInJava.接口;
-
+/**
+ * 通过继承，在接口中添加新的方法声明，
+ * 还可以通过继承在新街口中组合数个接口
+ * 这两种情况都可以获得新的接口
+ * */
 public class 扩展接口 {
     static void u(Monster b){b.menace();}
     static void v(DangerousMonster d){d.menace();d.destroy();}
@@ -37,3 +41,7 @@ class VeryBadVampire implements Vampire {
     public void kill() {    }
     public void drinkBlood(){   }
 }
+/**
+ * DangerousMonster时Monster的直接扩展，它产生了一个新接口。DragonZilla中实现了这个接口。
+ * 在Vampire中使用的语法仅适用于接口继承，一般情况下，只可以将extends用于单一类，但是可以引用多个基类接口。注意：接口需要用逗号一一分开即可
+ * */
